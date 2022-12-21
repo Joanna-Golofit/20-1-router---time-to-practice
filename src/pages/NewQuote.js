75e84486d7c791089dbfb794f0ce2d7d89d24ced
Joanna Gołofit <jgolofit@future-processing.com>
2022-12-21 15:27:02 +0100

    284. Preventing Possibly Unwanted Route Transitions with the "Prompt" Component

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,10 +1,19 @@
 import QuoteForm from '../components/quotes/QuoteForm';
 import React from 'react'
+import { useHistory } from 'react-router-dom';
 
 const NewQuote = () => {
+
+  const history = useHistory();
+  console.log("history", history)
+
   const addQuoteHandler = quoteData => {
-    console.log(quoteData)
+    console.log("addQuoteHandler", quoteData)
+    history.push('/quotes') // we are navigated away to /quotes
+    console.log("history", history)
+
   }
+
   return <QuoteForm onAddQuote={ addQuoteHandler} />
 }
 
